906d4b9169b374943c7234838607461bc7d15b84
magmafellow <magmafellow@gmail.com>
2025-06-16 19:29:48 +0300

    Cookies lesson passed #12

---
+++ archive-base/03_cookies/utils/constants.mjs
@@ -0,0 +1,10 @@
+
+export const mockUsers = [
+  { id: 1, username: 'anson', displayName: 'AnsonDi' },
+  { id: 2, username: 'magma', displayName: 'MagmaFellow' },
+  { id: 3, username: 'alice', displayName: 'Alice' },
+  { id: 4, username: 'tina', displayName: 'Tina' },
+  { id: 5, username: 'lina', displayName: 'Lina' },
+  { id: 6, username: 'gave', displayName: 'Gave' },
+  { id: 7, username: 'rex', displayName: 'Rex' },
+]
